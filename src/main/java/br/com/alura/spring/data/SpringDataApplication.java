58614046343ec18978c472e2fe34101ada6ee99f
Francisco de Assis <a124fr@gmail.com>
2021-06-30 16:59:41 -0300

    Minha implementação de atualizar um cargo

--- src/main/java/br/com/alura/spring/data/SpringDataApplication.java
+++ src/main/java/br/com/alura/spring/data/SpringDataApplication.java
@@ -1,19 +1,24 @@
 package br.com.alura.spring.data;
 
+import java.util.List;
+import java.util.Scanner;
+
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 import br.com.alura.spring.data.orm.Cargo;
-import br.com.alura.spring.data.repository.CargoRepository;
+import br.com.alura.spring.data.service.CrudCargoService;
 
 @SpringBootApplication
 public class SpringDataApplication implements CommandLineRunner {
 
-	private final CargoRepository repository;
+	private Boolean system = true;
 	
-	public SpringDataApplication(CargoRepository repository) {
-		this.repository = repository;
+	private final CrudCargoService cargoService;
+	
+	public SpringDataApplication(CrudCargoService cargoService) {
+		this.cargoService = cargoService;
 	}
 	
 	public static void main(String[] args) {
@@ -22,9 +27,36 @@ public class SpringDataApplication implements CommandLineRunner {
 
 	@Override
 	public void run(String... args) throws Exception {
-		Cargo cargo = new Cargo();
-		cargo.setDescricao("DESENVOLVEDOR DE SOFTWARE");
+		Scanner scan = new Scanner(System.in);
 		
-		repository.save(cargo);
+		while(system) {
+			System.out.println("Qual ação você quer executar");
+			System.out.println("0 - sair");
+			System.out.println("1 - cargo");
+			System.out.println("2 - atualizar cargo");
+			
+			int acao = scan.nextInt();
+			if(acao == 1) {
+				this.cargoService.inicial(scan);
+			} else if(acao == 2) {
+				System.out.println("Lista de Registro: ");
+				List<Cargo> cargos = this.cargoService.listar();
+				cargos.forEach(c->System.out.println(c.getId() + " - " + c.getDescricao()));
+				System.out.println("---------------------------------------------");
+				System.out.println("Digite um código de cargo?");
+				Integer codigoCargo = scan.nextInt();
+				System.out.println("Digite a nova descricao?");
+				String descricaoCargo = scan.next();
+				
+				Cargo cargo = new Cargo();
+				cargo.setId(codigoCargo);
+				cargo.setDescricao(descricaoCargo);
+				
+				this.cargoService.atualizar(cargo);				
+			} else {
+				system = false;
+			}
+			
+		}
 	}
 }
