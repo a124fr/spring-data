a446e98e70d9821c75035cee6665ed9c2c076bb4
Francisco de Assis <a124fr@gmail.com>
2021-07-02 15:41:16 -0300

    Specification com vários campos

--- src/main/java/br/com/alura/spring/data/service/RelatorioFuncionarioDinamico.java
+++ src/main/java/br/com/alura/spring/data/service/RelatorioFuncionarioDinamico.java
@@ -1,16 +1,22 @@
 package br.com.alura.spring.data.service;
 
+import java.math.BigDecimal;
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
 import java.util.List;
 import java.util.Scanner;
 
 import org.springframework.data.jpa.domain.Specification;
+import org.springframework.stereotype.Service;
 
 import br.com.alura.spring.data.orm.Funcionario;
 import br.com.alura.spring.data.repository.FuncionarioRepository;
 import br.com.alura.spring.data.specification.SpecificationFuncionario;
 
+@Service
 public class RelatorioFuncionarioDinamico {
 	
+	private final DateTimeFormatter formatter = DateTimeFormatter.ofPattern("dd/MM/yyyy");
 	private final FuncionarioRepository funcionarioRepository;
 	
 	public RelatorioFuncionarioDinamico(FuncionarioRepository funcionarioRepository) {
@@ -21,8 +27,42 @@ public class RelatorioFuncionarioDinamico {
 		System.out.println("Digite o nome: ");
 		String nome = scan.next();
 		
-		List<Funcionario> lista = this.funcionarioRepository
-				.findAll(Specification.where(SpecificationFuncionario.nome(nome)));
+		if(nome.equalsIgnoreCase("NULL")) {
+			nome = null;
+		}
 		
+		System.out.println("Digite o cpf: ");
+		String cpf = scan.next();
+		
+		if(cpf.equalsIgnoreCase("NULL")) {
+			cpf = null;
+		}
+		
+		System.out.println("Digite o salário: ");
+		BigDecimal salario = new BigDecimal(scan.next());
+		
+		if(salario.equals(BigDecimal.ZERO)) {
+			salario = null;
+		}
+		
+		System.out.println("Digite a data contratação: ");
+		String data = scan.next();
+		
+		LocalDate dataContratacao = null;
+		if(data.equalsIgnoreCase("NULL")) {
+			data = null;
+		} else {
+			dataContratacao = LocalDate.parse(data, formatter);
+		}
+		
+		List<Funcionario> lista = this.funcionarioRepository
+				.findAll(Specification.where(
+							SpecificationFuncionario.nome(nome))
+							.or(SpecificationFuncionario.cpf(cpf))
+							.or(SpecificationFuncionario.salario(salario))
+							.or(SpecificationFuncionario.dataContratacao(dataContratacao))						
+						);		
+		
+		lista.forEach(System.out::println);
 	}
 }
